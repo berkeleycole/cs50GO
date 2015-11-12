680ea172e55e8f20f4849122d9bb4505f0a57da2
Berkeley Cole <alyssamhope@gmail.com>
2015-11-12 15:52:05 -0800

    starts mario assignment

---
+++ gospace/src/mario.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+// this is a comment
+
+func main() {
+    fmt.Println("Please choose the hight of your pyramid! Any positive number no greater than 23 will do:\n")
+}
