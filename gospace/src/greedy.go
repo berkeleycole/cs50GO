764a0be1f297fe4c73d9f24338196caa83138549
Berkeley Cole <alyssamhope@gmail.com>
2015-11-13 12:24:15 -0800

    updates first half of the greedy change maker program

--- gospace/src/greedy.go
+++ gospace/src/greedy.go
@@ -3,15 +3,27 @@ package main
 import (
 	"fmt"
 	"errors"
-	"strings"
+)
+
+var (
+	NegativeNumErr = errors.New("You can't select a negative number")
 )
 
 func main() {
 	//ask user for a float(amount due in change)
-		//Continue asking for a number until a positive number is given
+	fmt.Println("How much in change? \n")
+	var change float32
+	fmt.Scan(&change)
 	
+	//Continue asking for a number until a positive number is given
+	for change < 0 {
+		fmt.Println(NegativeNumErr)
+		fmt.Println("Try Again:")
+		fmt.Scan(&change)
+	}
 	//multiply the float by 100 and truncate(setting $ amounts to cents)
-	
+	change = change*100
+	fmt.Printf("That is %.0f cents!\n", change)
 	//loop will break the input into 25's, 10's, 5's and 1's
 		//if i > 25
 			//subtract total by 25 and increase coin count by one
@@ -22,5 +34,5 @@ func main() {
 		//if i < 5 && i > 1
 			//subtract total by 1 and increase coin count by one
 			
-	//Print number of coins used to dispense the designated amount of 	change
+	//Print number of coins used to dispense the designated amount of change
 }
