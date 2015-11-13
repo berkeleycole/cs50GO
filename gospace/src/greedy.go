6d9aeeb3fa61b17efe79d3fa32a7a00d2112f560
Berkeley Cole <alyssamhope@gmail.com>
2015-11-13 11:28:35 -0800

    creates greedy.go assignment file with sudo code

---
+++ gospace/src/greedy.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"errors"
+	"strings"
+)
+
+func main() {
+	//ask user for a float(amount due in change)
+		//Continue asking for a number until a positive number is given
+	
+	//multiply the float by 100 and truncate(setting $ amounts to cents)
+	
+	//loop will break the input into 25's, 10's, 5's and 1's
+		//if i > 25
+			//subtract total by 25 and increase coin count by one
+		//if i < 25 && i > 10
+			//subtract total by 10 and increase coin count by one
+		//if i < 10 && i > 5
+			//subtract total by 5 and increase coin count by one
+		//if i < 5 && i > 1
+			//subtract total by 1 and increase coin count by one
+			
+	//Print number of coins used to dispense the designated amount of 	change
+}
